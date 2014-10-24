a5399d41defbea9e54344e96d8e26a5450d32cb3
Michiel <vanwijk.mc@gmail.com>
2014-10-24 14:48:43 +0200

    Styling facelift

--- app/views/news-item.js
+++ app/views/news-item.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.View.extend({
   templateName: 'views/news-item',  
   item: Ember.computed.alias('content'),
+  classNames: ['news-item'],
+
   pageOffset: Ember.computed.oneWay('parentView.pageOffset'),
 
   itemIndex: function() {
