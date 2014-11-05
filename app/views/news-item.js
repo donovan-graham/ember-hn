c6245f8d4e9dd4fb0078ddf66c6f07f9635f8677
Michiel <vanwijk.mc@gmail.com>
2014-11-05 17:26:01 +0200

    adds loaded item animation

--- app/views/news-item.js
+++ app/views/news-item.js
@@ -4,5 +4,18 @@ export default Ember.View.extend({
   templateName: 'views/news-item',
   // TODO render deleted template for item.deleted === true
   // example: https://hacker-news.firebaseio.com/v0/item/8505603.json?print=pretty
-  classNames: ['news-item'],
+  classNameBindings: [':news-item', 'itemLoaded:loaded'],
+
+  itemLoaded: false,
+
+  observeLoading: function() {
+		var _this = this;
+		if (this.get('content.item.isLoaded')) {
+			Ember.run.once(_this, function() {
+				_this.set('itemLoaded', true);
+			}, 100);
+		}
+  }.observes('content.item.isLoaded'),
+
+
 });
