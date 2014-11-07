63791f5d9e6a123c0f36f5059a1d3c5a44de1af8
Donovan Graham <donovan@platform7.com>
2014-11-08 00:41:26 +0200

    New item spinner and scrolltop on pagination

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -7,6 +7,16 @@ export default Ember.ArrayController.extend({
 
   stories: Ember.computed.alias('model'),
 
+  observePage: function() {
+    window.scrollTo(0,0);
+    // Ember.run.next(this, this._scrollTop);
+  }.observes('page'),
+
+  // _scrollTop: function() {
+  //   var timing = this.get('paginatedStories.length') * 50;
+  //   Ember.$('html,body').animate({ scrollTop: 0 }, timing, 'swing');
+  // },
+
   totalPages: function() {
     if (this.get('stories.length')) {
       return Math.ceil(this.get('stories.length') / this.get('limit'));
@@ -36,7 +46,7 @@ export default Ember.ArrayController.extend({
   paginatedStories: function() {
     var page = parseInt(this.get('page'), 10);
     var start = Math.max(0, (page - 1) * this.get('limit'));
-    var end = start + this.get('limit');
+    var end = Math.min(start + this.get('limit'), this.get('stories.length'));
     return this.get('stories').slice(start, end);     
   }.property('stories.@each', 'page', 'limit'),
 
--- app/views/news-item.js
+++ app/views/news-item.js
@@ -2,20 +2,6 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   templateName: 'views/news-item',
-  // TODO render deleted template for item.deleted === true
-  // example: https://hacker-news.firebaseio.com/v0/item/8505603.json?print=pretty
-  classNameBindings: [':news-item', 'itemLoaded:loaded'],
-
-  itemLoaded: false,
-
-  observeLoading: function() {
-		var _this = this;
-		if (this.get('content.item.isLoaded')) {
-			Ember.run.once(_this, function() {
-				_this.set('itemLoaded', true);
-			}, 100);
-		}
-  }.observes('content.item.isLoaded'),
-
-
+  classNameBindings: [':news-item', 'loaded'],
+  loaded: Ember.computed.alias('content.item.isLoaded'),
 });
