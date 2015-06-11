1bbc1abb26dcf943a8d4a3f43ec842cad2d94673
Donovan Graham <donovan@platform7.com>
2015-06-11 18:10:51 +0200

    Last of the route cleaning done

--- app/router.js
+++ app/router.js
@@ -6,14 +6,14 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('news', { path: 'news' });
+  this.route('topstories', { path: "new" });
+  this.route('askstories', { path: "ask" });
+  this.route('jobstories', { path: "job" });
+  this.route('showstories', { path: "show" });
+
   this.route('item', { path: "item/:item_id" });
   this.route('user', { path: "user/:user_id" });
 
-  this.route('askstories');
-  this.route('jobstories');
-  this.route('showstories');
-  this.route('topstories');
 });
 
 export default Router;
--- app/topstories/controller.js
+++ app/topstories/controller.js
@@ -4,8 +4,6 @@ import Pagination from 'hacker-news/mixins/pagination';
 var alias = Ember.computed.alias;
 
 export default Ember.ArrayController.extend(Pagination, {
-
   stories: alias('model'),
   paginate: alias('stories'),
-
 });
