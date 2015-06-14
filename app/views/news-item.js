5fe7fd4cac95c6d46a7e01a61605458e69da0eca
Donovan Graham <donovan@platform7.com>
2015-06-14 16:45:48 +0200

    Converting collection view into component

--- app/views/news-item.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-var alias = Ember.computed.alias;
-
-export default Ember.View.extend({
-  templateName: 'views/news-item',
-  classNameBindings: [':news-item', 'loaded'],
-  loaded: alias('content.item.isLoaded'),
-});
--- app/views/news-items.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import NewsItemView from 'hacker-news/views/news-item';
- 
-export default Ember.CollectionView.extend({
-  tagName: 'ul',
-  classNames: ['news-items'],
-  itemViewClass: NewsItemView
-});
-
