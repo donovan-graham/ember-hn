7c3c4c2f93d8c7e86a916974930fdf9d15653445
Donovan Graham <donovan@platform7.com>
2015-06-14 18:01:59 +0200

    Quick fix

--- app/user/serializer.js
+++ app/user/serializer.js
@@ -1,7 +1,9 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.FirebaseSerializer.extend({
-  normalizeHasMany: function() {
+  normalizeHasMany: function(typeClass, hash, relationship) {
+    // we sometimes get an empty dict for kids and we want an array, 'submitted' : {}
     var key = relationship.key;
     if (!Ember.isArray(hash[key])) {
       hash[key] = [];
