3fcf4576a4b6669e0b8f244c4ef4263680a02164
Donovan Graham <donovan@platform7.com>
2015-07-31 15:19:37 +0200

    Adding ember-cli-page-object to npm

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function(environment) {
     APP: { },
   };
 
-  ENV.stubFirebase = false;
+  ENV.stubFirebase = true;
 
   if (environment === 'development') {
     // ENV.stubFirebase = true;
